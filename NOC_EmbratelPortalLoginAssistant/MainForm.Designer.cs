6b033be23c6c70b621be3c924724ce1434853391
Fabiano <contatofasantiago@hotmail.com>
2024-09-20 13:54:27 -0300

    updade: aplicado nomeacao dos buttons e reajustado o tamanho/largura do form

--- NOC_EmbratelPortalLoginAssistant/MainForm.Designer.cs
+++ NOC_EmbratelPortalLoginAssistant/MainForm.Designer.cs
@@ -11,8 +11,6 @@ namespace NOC_EmbratelPortalLoginAssistant
 		private System.Windows.Forms.Button button2;
 		private System.Windows.Forms.Button button3;
 		private System.Windows.Forms.Button button4;
-		private System.Windows.Forms.Button button5;
-		private System.Windows.Forms.Button button6;
 		
 		protected override void Dispose(bool disposing)
 		{
@@ -30,8 +28,6 @@ namespace NOC_EmbratelPortalLoginAssistant
 			this.button2 = new System.Windows.Forms.Button();
 			this.button3 = new System.Windows.Forms.Button();
 			this.button4 = new System.Windows.Forms.Button();
-			this.button5 = new System.Windows.Forms.Button();
-			this.button6 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// button1
@@ -40,7 +36,7 @@ namespace NOC_EmbratelPortalLoginAssistant
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(88, 66);
 			this.button1.TabIndex = 0;
-			this.button1.Text = "button1";
+			this.button1.Text = "JSL";
 			this.button1.UseVisualStyleBackColor = true;
 			// 
 			// button2
@@ -49,7 +45,7 @@ namespace NOC_EmbratelPortalLoginAssistant
 			this.button2.Name = "button2";
 			this.button2.Size = new System.Drawing.Size(88, 66);
 			this.button2.TabIndex = 1;
-			this.button2.Text = "button2";
+			this.button2.Text = "INTERMÉDICA";
 			this.button2.UseVisualStyleBackColor = true;
 			// 
 			// button3
@@ -58,7 +54,7 @@ namespace NOC_EmbratelPortalLoginAssistant
 			this.button3.Name = "button3";
 			this.button3.Size = new System.Drawing.Size(88, 66);
 			this.button3.TabIndex = 2;
-			this.button3.Text = "button3";
+			this.button3.Text = "MOVIDA";
 			this.button3.UseVisualStyleBackColor = true;
 			// 
 			// button4
@@ -67,35 +63,15 @@ namespace NOC_EmbratelPortalLoginAssistant
 			this.button4.Name = "button4";
 			this.button4.Size = new System.Drawing.Size(88, 66);
 			this.button4.TabIndex = 3;
-			this.button4.Text = "button4";
+			this.button4.Text = "CENTAURO";
 			this.button4.UseVisualStyleBackColor = true;
-			// 
-			// button5
-			// 
-			this.button5.Location = new System.Drawing.Point(388, 12);
-			this.button5.Name = "button5";
-			this.button5.Size = new System.Drawing.Size(88, 66);
-			this.button5.TabIndex = 4;
-			this.button5.Text = "button5";
-			this.button5.UseVisualStyleBackColor = true;
-			// 
-			// button6
-			// 
-			this.button6.Location = new System.Drawing.Point(482, 12);
-			this.button6.Name = "button6";
-			this.button6.Size = new System.Drawing.Size(88, 66);
-			this.button6.TabIndex = 5;
-			this.button6.Text = "button6";
-			this.button6.UseVisualStyleBackColor = true;
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.DarkGray;
-			this.ClientSize = new System.Drawing.Size(582, 372);
-			this.Controls.Add(this.button6);
-			this.Controls.Add(this.button5);
+			this.ClientSize = new System.Drawing.Size(396, 89);
 			this.Controls.Add(this.button4);
 			this.Controls.Add(this.button3);
 			this.Controls.Add(this.button2);
