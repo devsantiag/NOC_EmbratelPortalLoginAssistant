ab669bdd6e3d3da36add65757a3cbce7b906c8c4
Fabiano <contatofasantiago@hotmail.com>
2024-09-20 13:19:31 -0300

    remocao: comentarios iniciais

--- NOC_EmbratelPortalLoginAssistant/MainForm.cs
+++ NOC_EmbratelPortalLoginAssistant/MainForm.cs
@@ -3,31 +3,17 @@
  * User: fjstavares
  * Date: 20/09/2024
  * Time: 11:38
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
-using System.Collections.Generic;
-using System.Drawing;
 using System.Windows.Forms;
 
 namespace NOC_EmbratelPortalLoginAssistant
 {
-	/// <summary>
-	/// Description of MainForm.
-	/// </summary>
 	public partial class MainForm : Form
 	{
 		public MainForm()
 		{
-			//
-			// The InitializeComponent() call is required for Windows Forms designer support.
-			//
 			InitializeComponent();
-			
-			//
-			// TODO: Add constructor code after the InitializeComponent() call.
-			//
 		}
 	}
 }
--- NOC_EmbratelPortalLoginAssistant/Program.cs
+++ NOC_EmbratelPortalLoginAssistant/Program.cs
@@ -1,24 +1,14 @@
 ﻿/*
- * Created by SharpDevelop.
- * User: fjstavares
  * Date: 20/09/2024
  * Time: 11:38
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
 using System.Windows.Forms;
 
 namespace NOC_EmbratelPortalLoginAssistant
 {
-	/// <summary>
-	/// Class with program entry point.
-	/// </summary>
 	internal sealed class Program
 	{
-		/// <summary>
-		/// Program entry point.
-		/// </summary>
 		[STAThread]
 		private static void Main(string[] args)
 		{
