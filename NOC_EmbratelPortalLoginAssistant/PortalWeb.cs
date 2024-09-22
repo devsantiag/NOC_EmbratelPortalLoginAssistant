63c90947c1be26eb3700f4f2238f151d0c7bed8f
Fabiano <contatofasantiago@hotmail.com>
2024-09-22 14:14:35 -0300

    criado um novo formulário externo

---
+++ NOC_EmbratelPortalLoginAssistant/PortalWeb.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: fjstavares
+ * Date: 22/09/2024
+ * Time: 14:13
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace NOC_EmbratelPortalLoginAssistant
+{
+	/// <summary>
+	/// Description of PortalWeb.
+	/// </summary>
+	public partial class PortalWeb : Form
+	{
+		public PortalWeb()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
